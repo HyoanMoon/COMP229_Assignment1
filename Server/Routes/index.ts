39029d60c11761f2fb6c2106f60a4ba7c7448b61
문효안 <hyoanmoon@munhyoan-ui-MacBookAir.local>
2022-10-17 22:31:24 -0400

    completed basic tamplate

--- Server/Routes/index.ts
+++ Server/Routes/index.ts
@@ -1,12 +1,39 @@
-import { NextFunction, Router } from "express";
-import { Module } from "module";
-
-import express from'express';
+import express from 'express';
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req: express.Request, res: express.Response , next: express.NextFunction) {
-  res.render('index', { title: 'Express' });
+/* Display home page. */
+router.get('/', function(req: express.Request, res: express.Response , next: express.NextFunction) 
+{
+  res.render('index', { title: 'Home', page: 'home'});
 });
+/* Display home page. */
+router.get('/home', function(req: express.Request, res: express.Response , next: express.NextFunction) 
+{
+  res.render('index', { title: 'Home', page: 'home'});
+});
+/* Display about page. */
+router.get('/about', function(req: express.Request, res: express.Response , next: express.NextFunction) 
+{
+  res.render('index', { title: 'About Me', page: 'about'});
+});
+
+/* Display project page. */
+router.get('/projects', function(req: express.Request, res: express.Response , next: express.NextFunction) 
+{
+  res.render('index', { title: 'My Projects', page: 'projects'});
+});
+
+/* Display services page. */
+router.get('/services', function(req: express.Request, res: express.Response , next: express.NextFunction) 
+{
+  res.render('index', { title: 'Services', page: 'services'});
+});
+
+/* Display contact page. */
+router.get('/contact', function(req: express.Request, res: express.Response , next: express.NextFunction) 
+{
+  res.render('index', { title: 'Contact', page: 'contact'});
+});
+
 
 export default router;
